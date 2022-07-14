a207a43b3f20391e76aaf1d03c2e50fd6924f339
Alberto Corona <muqui@hotmail.com>
2022-07-14 18:43:15 -0500

    exercises IV

--- test.js
+++ test.js
@@ -1,21 +1,6 @@
-const { numeroMasGrande } = require("./04-JS-III/homework/homework");
-
-function todosIguales(arreglo) {
-    //Escriba la función todosIguales, que indique si todos los elementos de un arreglo son iguales:
-    //retornar true, caso contrario retornar false.
-    //Escribe tu código aquí  
-    valor = arreglo[0];
-
-    for (let i = 0; i < arreglo.length ; i++) {
-        console.log(i);
-        if(valor == arreglo[i]){
-         console.log(i);
-         return true;
-        }
-      }
-    
-    return false;
-  } 
-
-  var resultado = todosIguales([1,1,1,1,1,1,1,1]);
-  console.log(resultado);
+persona = {
+  nombre: 'Alberto',
+  edad: 37,
+  deporte : 'running'
+}
+console.log(persona['edad']);
